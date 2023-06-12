3bbb6b16668af9f544a9f212f643cf640c38f060
Beau <beaulazear@gmail.com>
2023-06-11 20:50:02 -0400

    commit tree 7e9a37af97f3fc1f6b3c5be2261c886a1ef3c8cd

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :is_clickbaity
+
+    def is_clickbaity
+        # titles = ["won't believe", "guess", "secret", "top #{integer}"]
+
+        x = /won't believe|guess|secret|top [0-9]/
+        if title
+            unless x.match?(title.downcase)
+                errors.add(:title, "Please only use captivating titles that include keywords such as Guess, Secret, Won't Believe, or top (some number)")
+            end
+        end
+    end
 end
